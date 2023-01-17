16c0d074e8a9225786fb2e4c3f703fb6dc86c9b5
Sam Jackson <samjacksonjim@outlook.com>
2023-01-17 19:12:57 +0530

    basic styles and layout with bootstrap added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const Campground = require('./models/campground')
+const ejsMate = require('ejs-mate')
 
 const path = require('path')
 const app = express()
@@ -22,9 +23,11 @@ db.once('open', () => {
     console.log('database connected')
 })
 
-
+app.engine('ejs',ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+
+
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({extended:true}))
 
